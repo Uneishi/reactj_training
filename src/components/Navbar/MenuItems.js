f1dfc09df381adf2172d5eda79fdbabcd8e61f16
Uneishi <casusbelli01@gmail.com>
2021-01-15 16:21:27 +0100

    first commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Portefolio',
+        url: '/portfolio',
+        cName:'nav-links'
+    },
+    {
+        title: 'A Propos',
+        url: '/faq',
+        cName:'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '/contact',
+        cName:'nav-links'
+    }
+]
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "../components/Navbar/Navbar";
+import Form from "../components/Form/Form";
+
+const Contact = () =>{
+    return (
+        <div className="App">
+         <Navbar/>
+         <Form/>
+        </div>
+      );
+}
+
+export default Contact;
